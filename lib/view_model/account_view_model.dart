e29f2a3d7c6f829f7aa1033b2f10cf083d17a607
xhzq233e <xhzq233@qq.com>
2023-12-05 11:16:18 +0800

    Hint: AccountViewModel

--- lib/view_model/account_view_model.dart
+++ lib/view_model/account_view_model.dart
@@ -1,14 +1,37 @@
 /// pivot_chat - account_view_model
 /// Created by xhz on 11/9/23
 import 'package:flutter/foundation.dart';
+import 'package:flutter_openim_sdk/flutter_openim_sdk.dart';
 
 /// 放在顶层是因为很多地方需要使用，需要扩大作用域。
 /// TODO: 监听每个用户状态的变化，比如用户的在线、信息等。
 /// 比如自身状态的更新
 /// API(https://doc.rentsoft.cn/zh-Hans/sdks/listener/userListener)
-class AccountViewModel extends ChangeNotifier {
+class AccountViewModel extends OnUserListener with ChangeNotifier {
   final String userID;
 
-  // 指定监听的用户ID
-  AccountViewModel({required this.userID});
+  UserInfo? userInfo;
+
+  // 指定监听的用户ID，目前应该只有自己..
+  AccountViewModel({required this.userID}) {
+    OpenIM.iMManager.userManager.setUserListener(this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    OpenIM.iMManager.userManager.setUserListener(OnUserListener());
+  }
+
+  @override
+  void selfInfoUpdated(UserInfo info) {
+    userInfo = info;
+    notifyListeners();
+  }
+
+  @override
+  void userStatusChanged(UserStatusInfo info) {
+    // TODO: implement userStatusChanged
+    notifyListeners();
+  }
 }
